d44adfe3cf6307ac9e9069199ce8caab313a61c4
Shecho Ruiz <shecho27@gmail.com>
2022-04-26 23:13:09 -0500

    Definición de tipos básicos

--- src/typescript/TiposBasicos.tsx
+++ src/typescript/TiposBasicos.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-
 const TiposBasicos = () => {
+	const nombre: string = "Sergio";
+  const edad: number = 31
+	const estaActivo: boolean = true
+	const poderes: string[] = ['Velocidad', 'Volar', 'Respirar en el agua']
+	const array: string[] = []
+
+	// array.push(1)
+	array.push('Que se dice?')
+	// array.push(false)
+
   return (
     <>
-    <h3>Tipos básicos</h3></>
-  )
-}
+      <h3>Tipos básicos</h3>
+      {nombre}, {edad}, {{estaActivo} ? 'activo ' : 'inactivo '}
+			{poderes.join(', ')}
+		</>
+  );
+};
 
-export default TiposBasicos
+export default TiposBasicos;
