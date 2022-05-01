76e15b41c10c041f83f96c66f5374789412f1219
Shecho Ruiz <shecho27@gmail.com>
2022-05-01 15:50:50 -0500

    Definición de hook useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@
 // import ObjetosLiterales from "./typescript/ObjetosLiterales"
 // import Funciones from "./typescript/Funciones"
 // import Contador from "./components/Contador";
-
-import ContadorConHook from "./components/ContadorConHook";
+// import ContadorConHook from "./components/ContadorConHook";
+import Login from "./components/Login";
 
 const App = () => {
   return (
@@ -14,7 +14,8 @@ const App = () => {
       {/* <ObjetosLiterales/> */}
       {/* <Funciones/> */}
       {/* <Contador /> */}
-      <ContadorConHook />
+      {/* <ContadorConHook /> */}
+      <Login />
     </div>
   );
 };
---
+++ src/components/Login.tsx
@@ -0,0 +1,101 @@
+import { useEffect, useReducer } from "react";
+
+interface AuthState {
+  validando: boolean;
+  token: string | null;
+  username: string;
+  nombre: string;
+}
+
+const initialState: AuthState = {
+  validando: true,
+  token: null,
+  username: "",
+  nombre: "",
+};
+
+type LoginPayload = {
+  username: string;
+  nombre: string;
+};
+
+type AuthAction = { type: "logout" } | { type: "login"; payload: LoginPayload };
+
+const authReducer = (state: AuthState, action: AuthAction): AuthState => {
+  switch (action.type) {
+    case "logout":
+      return {
+        validando: false,
+        token: null,
+        username: "",
+        nombre: "",
+      };
+    case "login":
+      const { username, nombre } = action.payload;
+      return {
+        validando: false,
+        token: "ABC123",
+        username: username,
+        nombre: nombre,
+      };
+    default:
+      return state;
+  }
+};
+
+const Login = () => {
+  const [{ validando, token, nombre }, dispatch] = useReducer(
+    authReducer,
+    initialState
+  );
+
+  useEffect(() => {
+    setTimeout(() => {
+      dispatch({ type: "logout" });
+    }, 1500);
+  }, []);
+
+  const login = () => {
+    dispatch({
+      type: "login",
+      payload: { nombre: "Sergio", username: "Shecho_Ruiz" },
+    });
+  };
+
+  const logout = () => {
+    dispatch({ type: "logout" });
+  };
+
+  if (validando) {
+    return (
+      <>
+        <h3>Login</h3>
+
+        <div className="alert alert-info">Validando...</div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <h3>Login</h3>
+      {token ? (
+        <div className="alert alert-success">Autenticado como: {nombre}</div>
+      ) : (
+        <div className="alert alert-danger">No autenticado</div>
+      )}
+
+      {token ? (
+        <button className="btn btn-danger" onClick={logout}>
+          Logout
+        </button>
+      ) : (
+        <button className="btn btn-primary" onClick={login}>
+          Login
+        </button>
+      )}
+    </>
+  );
+};
+
+export default Login;
