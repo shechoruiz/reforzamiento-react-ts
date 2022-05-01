ac987a1f51f33e251d253e15f2eea83f53cabab5
Shecho Ruiz <shecho27@gmail.com>
2022-05-01 17:28:11 -0500

    Definición de formularios

---
+++ src/components/Formularios.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+const Formularios = () => {
+  const [formulario, setFormulario] = useState({
+    email: "test@test.com",
+    password: "123456",
+  });
+
+  const onChange = (value: string, campo: string) => {
+    setFormulario({
+      ...formulario,
+      [campo]: value,
+    });
+  };
+
+  return (
+    <>
+      <h3>Formularios</h3>
+      <input
+        type="email"
+        className="form-control"
+        placeholder="Email"
+        value={formulario.email}
+        onChange={({ target }) => onChange(target.value, "email")}
+      />
+      <input
+        type="text"
+        className="form-control mt-2 mb-2"
+        placeholder="Password"
+        value={formulario.password}
+        onChange={({ target }) => onChange(target.value, "password")}
+      />
+      <code>
+        <pre>{JSON.stringify(formulario, null, 2)}</pre>
+      </code>
+    </>
+  );
+};
+
+export default Formularios;
